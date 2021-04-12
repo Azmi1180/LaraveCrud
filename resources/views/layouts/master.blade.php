6f3cc6e44f36b5b7815add97ab15772fd622f09b
Muhammad Azmi <muhammadazmi4456@gmail.com>
2021-04-12 12:17:56 +0700

    menambah master layouts

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Table data siswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <style>
+
+    </style>
+</head>
+<body>
+        
+    @include('layouts.navigation')
+    {{-- Section Table --}}
+    
+    <div class="container mt-5">
+        @yield('content')
+    </div>
+         {{-- Script Javascript --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
